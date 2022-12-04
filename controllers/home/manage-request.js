0cda0730c980c9e2101eeff6e2a620a64fe8fde0
Jayaram <jayvenkat1998@gmail.com>
2022-12-04 17:57:39 -0500

    manage request

--- controllers/home/manage-request.js
+++ controllers/home/manage-request.js
@@ -3,6 +3,7 @@ import * as productReqDao from '../../dao/product_request-dao.js'
 const ManageProductRequestController = (app) => {
     app.get('/api/products/requests', getProductRequests)
     app.get('/api/products/request/approve/:id',approveRequest)
+    app.get('/api/products/request/reject/:id',rejectRequest)
 }
 
 
@@ -15,6 +16,12 @@ const approveRequest = async (req, res) => {
     const req_id = req.params.id;
     console.log(req_id)
     const prodRequests = await productReqDao.approveRequest(req_id);
+    getProductRequests(req,res);
+}
+
+const rejectRequest = async (req, res) => {
+    const req_id = req.params.id;
+    const prodRequests = await productReqDao.rejectRequest(req_id);
     getProductRequests(req,res);
 }
 
